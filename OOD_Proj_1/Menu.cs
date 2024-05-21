ef2b6b79f22180571a5f983815ee8bed6651ef60
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2024-05-21 23:55:51 +0200

    cleanup

--- OOD_Proj_1/Menu.cs
+++ OOD_Proj_1/Menu.cs
@@ -39,13 +39,13 @@ namespace OOD_Proj_1
                         Console.WriteLine("Invalid Command");
                     }
                 }
-                }
-            }
-            public static void StopAll(ProductLists p)
-            {
-                ServerSimulator.StopServer();
-                if (GuiApp.UpdateGuiThread != null && GuiApp.UpdateGuiThread.IsAlive)
-                    Console.WriteLine("In order to fully exit application close GUI");
             }
         }
+        public static void StopAll(ProductLists p)
+        {
+            ServerSimulator.StopServer();
+            if (GuiApp.UpdateGuiThread != null && GuiApp.UpdateGuiThread.IsAlive)
+                Console.WriteLine("In order to fully exit application close GUI");
+        }
     }
+}
