352515c8c64278a2240eafa7bdd50bf247b8a673
Kobiuy <132697518+Kobiuy@users.noreply.github.com>
2024-05-21 23:51:26 +0200

    Small fix

--- OOD_Proj_1/Menu.cs
+++ OOD_Proj_1/Menu.cs
@@ -20,7 +20,7 @@ namespace OOD_Proj_1
             string UserInput = "";
             Action<ProductLists> method;
             Console.WriteLine("Write \"print\" to make a snapshot, \"gui\" for GUI, \"report\" or \"exit\" to exit menu.");
-            Console.WriteLine("You can queries \"Display\" \"Add\" \"Delete\" and \"Update\" as well");
+            Console.WriteLine("You can use queries like \"Display\" \"Add\" \"Delete\" and \"Update\" as well");
             while (UserInput != "exit")
             {
                 UserInput = Console.ReadLine();
